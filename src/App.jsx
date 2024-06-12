3f5c544aaa2c45723382f561f798a1bbafe940e8
Harshit Gupta <Harshitg1710@gmail.com>
2024-06-12 17:49:11 +0530

    initial Routes Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import HomePage from './pages/HomePage'
+import NotFound from './pages/NotFound'
+import Movies from './pages/Movies'
+import {Routes , Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <div>
+    <div className = 'App'>
       Main page
+      <Routes >
+      
+      <Route path='/' element={<HomePage />} />
+      <Route path='/movies' element={<Movies />} />
+      <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   )
 }
